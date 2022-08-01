09ee93f56b9becbc4dd5a71f661033855e86665c
Andrew <andrew@andrewsmith.org>
2022-07-31 23:14:22 -0400

    Fetch player names at start

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,6 +2,17 @@
 
 class Board
   def initialize
+    fetch_player_names
+  end
+
+  def fetch_player_names
+    puts 'What\'s player one\'s name?'
+    @player1 = gets.chomp
+    puts 'What\'s player two\'s name?'
+    @player2 = gets.chomp
+  end
+
+  def create_board
     @board = [
       ['   ', '|', '   ', '|', '   ', "\n"],
       ['---', '+', '---', '+', '---', "\n"],
@@ -19,7 +30,7 @@ class Board
   end
 
   def player_turn
-    puts 'Player 1\'s turn!'
+    puts "#{@player1}'s turn!"
     puts 'Options:'
     puts 'Enter a number 1-9 to play.'
     puts 'Enter \'key\' to see which number corresponds to which square.'
