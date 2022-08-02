107fc5d011eec3009c5fa6cb53cf053767c45545
Andrew <andrew@andrewsmith.org>
2022-08-01 22:16:27 -0400

    Draw player move on board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,9 +4,9 @@ class Board
   def initialize
     fetch_player_names
     create_board
-    play_turn   # add a victory/draw checker function, invoke at end of play_turn to set a flag as true if game is over
-                # run play_turn inside an unless loop that checks whether it has become true that game is complete
-                # at end of turn, invoke a 'switch player' function to change between P1 and P2
+    play_turn # add a victory/draw checker function, invoke at end of play_turn to set a flag as true if game is over
+    # run play_turn inside an unless loop that checks whether it has become true that game is complete
+    # at end of turn, invoke a 'switch player' function to change between P1 and P2
   end
 
   def fetch_player_names
@@ -37,8 +37,9 @@ class Board
 
   def play_turn
     player_turn_instructions
-    player_move = player_turn_options
-    puts player_move
+    move = player_turn_options
+    draw_move(move)
+    show_board
   end
 
   def player_turn_instructions
@@ -83,6 +84,34 @@ class Board
   def show_key
     display_board = @key.join
     puts display_board
+  end
+
+  def draw_move(number)
+    location = translate_input(number)
+    @board[location[0]][location[1]] = ' X '
+  end
+
+  def translate_input(number)
+    case number
+    when '1'
+      [0, 0]
+    when '2'
+      [0, 2]
+    when '3'
+      [0, 4]
+    when '4'
+      [2, 0]
+    when '5'
+      [2, 2]
+    when '6'
+      [2, 4]
+    when '7'
+      [4, 0]
+    when '8'
+      [4, 2]
+    when '9'
+      [4, 4]
+    end
   end
 end
 
